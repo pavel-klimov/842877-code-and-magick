2048c4b39cdda40dc3f31a0b4391f2999d63349e
Pavel <pavelklimovmail@yandex.ru>
2018-09-03 19:08:32 +0300

    add statistics players output

--- js/stat.js
+++ js/stat.js
@@ -4,18 +4,21 @@ var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
-var GAP = 10;
-var FONT_GAP = 15;
-var TEXT_WIDTH = 50;
-var BAR_HEIGHT = 40;
-var barWidth = CLOUD_WIDTH - GAP - TEXT_WIDTH - GAP;
+var CLOUD_GAP = 10;
+var FONT_GAP = 4;
+var FONT_SIZE = 16;
+var fontLength = FONT_SIZE + FONT_GAP;
+var BAR_WIDTH = 40;
+var BAR_HEIGHT = 150;
+var BAR_GAP = 50;
+var START_POSITION = 10;
 
-var renderCloud = function(ctx, x, y, color) {
+var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-var getMaxElement = function(arr) {
+var getMaxElement = function (arr) {
   var maxElement = arr[0];
 
   for (var i = 0; i < arr.length; i++) {
@@ -27,16 +30,39 @@ var getMaxElement = function(arr) {
   return maxElement;
 };
 
-window.renderStatistics = function(ctx, players, times) {
-  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.3)');
+window.renderStatistics = function (ctx, players, times) {
+  var inputPosition = START_POSITION;
+  var pointPixelCost = BAR_HEIGHT / getMaxElement(times);
+
+  // Рисуем облако с тенью:
+  renderCloud(ctx, CLOUD_X + CLOUD_GAP, CLOUD_Y + CLOUD_GAP, 'rgba(0, 0, 0, 0.3)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
+  // Настраиваем стиль текста:
   ctx.fillStyle = '#000';
+  ctx.font = FONT_SIZE + 'px PT Mono';
 
-  var maxTime = getMaxElement(times);
+  // Выводим первые строки текста:
+  inputPosition += fontLength;
+  ctx.fillText('Ура вы победили!', CLOUD_X + fontLength, CLOUD_Y + inputPosition);
+  inputPosition += fontLength;
+  ctx.fillText('Список результатов:', CLOUD_X + fontLength, CLOUD_Y + inputPosition);
+  inputPosition += FONT_GAP;
 
+  // Рисуем колонки гистограммы:
   for (var i = 0; i < players.length; i++) {
-    ctx.fillText(players[i], CLOUD_X + GAP, CLOUD_Y + GAP + FONT_GAP + (GAP + BAR_HEIGHT) * i);
-    ctx.fillRect(CLOUD_X + GAP + TEXT_WIDTH, CLOUD_Y + GAP + (GAP + BAR_HEIGHT) * i, (barWidth * times[i]) / maxTime, BAR_HEIGHT);
+    // Настраиваем высоту и положение текущей колонки:
+    var columnHeigth = pointPixelCost * times[i];
+    var xPosition = CLOUD_X + BAR_GAP + (BAR_GAP + BAR_WIDTH) * i;
+    var yPosition = CLOUD_Y + inputPosition + FONT_GAP + fontLength + BAR_HEIGHT - columnHeigth;
+
+    // Выводим легенду выбранной колонки:
+    ctx.fillStyle = '#000';
+    ctx.fillText(Math.round(times[i]), xPosition, yPosition - FONT_GAP);
+    ctx.fillText(players[i], xPosition, yPosition + columnHeigth + fontLength);
+
+    // Переключаем цвет колонки и выводим её:
+    ctx.fillStyle = (players[i] === 'Вы') ? 'rgba(255, 0, 0, 1)' : 'hsl(240, ' + (Math.random() * 50 + 25) + '%, ' + (Math.random() * 50 + 25) + '%)';
+    ctx.fillRect(xPosition, yPosition, BAR_WIDTH, columnHeigth);
   }
 };
